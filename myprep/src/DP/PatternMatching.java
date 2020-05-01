03ae75713dff23c8c6eb3d30af37200e82fe9dfc
navaneethakrishnan.p <navaneethakrishnan.p@zohocorp.com>
2020-05-01 16:38:48 +0530

    dynamic programming approach added

--- myprep/src/DP/PatternMatching.java
+++ myprep/src/DP/PatternMatching.java
@@ -1,3 +1,6 @@
+//$Id$
+package myprep;
+
 public class PatternMatching 
 {
 	/**
@@ -21,9 +24,9 @@ public class PatternMatching
 	 */
 	public static void main(String [] args)
 	{
-		String text = "c";
+		String text = "aab";
 		
-		String pattern = "c*a*";
+		String pattern = "c*a*b";
 		
 		boolean isRegMatch = isPatternMatches(text,pattern);
 		
@@ -32,6 +35,8 @@ public class PatternMatching
 
 	private static boolean isPatternMatches(String text, String pattern) 
 	{
+		System.out.println("text " + text + " , " + "pattern " + pattern);
+		
 		if(pattern.isEmpty()) //base case
 		{
 			return text.isEmpty();
@@ -48,6 +53,37 @@ public class PatternMatching
 		{
 			return isFirstMatch && isPatternMatches(text.substring(1), pattern.substring(1));
 		}
+	}
+	
+	//constructing the same concept in dp 
+	public static boolean isMatch(String text, String pattern) 
+	{
+	    
+	    boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+	    
+	    dp[text.length()][pattern.length()] = true;
+
+	    
+	    for (int i = text.length(); i >= 0; i--) 
+	    {
+	        for (int j = pattern.length(); j >= 0; j--) 
+	        {
+	            
+	            if(i==text.length() &&j==pattern.length() ) 
+	            continue;
+
+	            boolean first_match = (i < text.length() && j < pattern.length()  && ( pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.') );
+	            if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') 
+	            {
+	                dp[i][j] = dp[i][j + 2] || first_match && dp[i + 1][j];
+	            } 
+	            else 
+	            {
+	                dp[i][j] = first_match && dp[i + 1][j + 1];
+	            }
+	        }
+	    }
+	    return dp[0][0];
 	}
 
 }
