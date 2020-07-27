78f137f52ff4fab78746d498c8ba9d2eb1679b83
NavaneethaKrishnanPandiarajan <32923750+NavaneethaKrishnanPandiarajan@users.noreply.github.com>
2020-07-27 16:10:04 +0530

    Update RemoveAdjacentDuplicates.java

--- arraysProgram/RemoveAdjacentDuplicates.java
+++ arraysProgram/RemoveAdjacentDuplicates.java
@@ -5,64 +5,48 @@ import java.util.Stack;
 
 public class RemoveAdjacentDuplicates 
 {
-	public static void removeDuplicates(int a[])
+	
+	public static String removeDuplicates(String s) 
 	{
-		Stack<Integer> s = new Stack<Integer>();
-		int length = a.length;
-		
-		int ctrlVariable = 0;
-		
+		if(s == null || s.isEmpty())
+		{
+			return s;
+		}
+
+		Deque <Character> deque = new LinkedList<Character>();
+		int length = s.length();
+		char a[] = s.toCharArray();
+
 		for (int i=0; i<length; i++)
 		{
-			if( s.isEmpty() )
+			if( !deque.isEmpty() && deque.peekLast() == a[i])
 			{
-				s.push(a[i]);
+				while(  !deque.isEmpty() && deque.peekLast() == a[i] )
+				{
+					deque.removeLast();
+				}
 			}
-			
 			else
 			{
-								
-				if( ctrlVariable == 1)
-				{
-					if(s.peek() != a[i])
-					{
-						s.pop();
-						
-						ctrlVariable = 0;
-					}
-				}
-						
-				if(s.peek() == a[i])
-				{
-					s.pop();
-					
-					ctrlVariable=1;
-					
-					if( i < length -1)
-					{
-						s.push(a[i]);
-					}
-				}
-				else
-				{
-					s.push(a[i]);
-				}
-				
+				deque.addLast(a[i]);
 			}
-			
 		}
-		
-		System.out.println(s);
+
+		StringBuilder builder = new StringBuilder();
+
+		while(!deque.isEmpty())
+		{
+			builder.append(deque.peekFirst());
+			deque.removeFirst();
+		}
+
+		return builder.toString();
 	}
 	
 	public static void main(String [] args)
 	{
-		int a[] = {1,5,6,8,8,8,0,1,1,0,6,5};
-		
-		int b[] = {1,9,6,8,8,8,0,1,1,0,6,5};
-		
-		removeDuplicates(a);
-		
-		removeDuplicates(b);
+		String a = "abbaca";
+		System.out.print(removeDuplicates(a));
 	}
+
 }
